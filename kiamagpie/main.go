114e1ae741732b0c0d4aa1ab341075e6f7b7e4cc
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-03-09 19:38:49 -0400

    v0.1.501
    pull from upstream kiamagpie - add new config values for tls versions

--- kiamagpie/main.go
+++ kiamagpie/main.go
@@ -31,7 +31,7 @@ import (
         "gopkg.in/yaml.v3"
 )
 
-const magpieVersion = "0.1.500"
+const magpieVersion = "0.1.501"
 const defaultHostKey = "__default__"
 
 const (
@@ -46,16 +46,18 @@ const (
 
 type MagpieConfig struct {
         Magpie struct {
-                QUICEnabled       bool        `yaml:"quic"`
-                TLSEnabled        bool        `yaml:"tls"`
-                HTTPEnabled       bool        `yaml:"http"`
-                HSTS              bool        `yaml:"strict_transport_security"`
-                CacheAgeSecs      int         `yaml:"cache_age_seconds"`
-                RAMLimitPercent   float64     `yaml:"ram_limit_percent"`
-                DomainsTLS        interface{} `yaml:"domains_tls"`
-                DomainsQUIC       interface{} `yaml:"domains_quic"`
-                DomainsHTTP       interface{} `yaml:"domains_http"`
-                DefaultWebContent string      `yaml:"default_web_content"`
+                QUICEnabled         bool        `yaml:"quic"`
+                TLSEnabled          bool        `yaml:"tls"`
+                HTTPEnabled         bool        `yaml:"http"`
+                HSTS                bool        `yaml:"strict_transport_security"`
+                CacheAgeSecs        int         `yaml:"cache_age_seconds"`
+                RAMLimitPercent     float64     `yaml:"ram_limit_percent"`
+                MinTLSVersion       uint16      `yaml:"min_tls_version"`
+                ClientMinTLSVersion uint16      `yaml:"client_min_tls_version"`
+                DomainsTLS          interface{} `yaml:"domains_tls"`
+                DomainsQUIC         interface{} `yaml:"domains_quic"`
+                DomainsHTTP         interface{} `yaml:"domains_http"`
+                DefaultWebContent   string      `yaml:"default_web_content"`
         } `yaml:"kiamagpie"`
 }
 
@@ -782,7 +784,7 @@ func tlsConfigForHost(vh VirtualHost) (*tls.Config, error) {
         }
 
         return &tls.Config{
-                MinVersion: tls.VersionTLS13,
+                MinVersion: magpieConfig.Magpie.MinTLSVersion,
                 CurvePreferences: []tls.CurveID{
                         tls.X25519MLKEM768,
                         tls.X25519,
@@ -1358,7 +1360,7 @@ func fetchRemote(origin *url.URL, reqPath string) ([]byte, string, int, error) {
 
 func strictHTTPClient() *http.Client {
         tlsConf := &tls.Config{
-                MinVersion: tls.VersionTLS13,
+                MinVersion: magpieConfig.Magpie.ClientMinTLSVersion,
                 CurvePreferences: []tls.CurveID{
                         tls.X25519MLKEM768,
                         tls.X25519,
