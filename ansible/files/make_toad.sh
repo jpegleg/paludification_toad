97a73b2b6f262a22b398be4eda8990ce32583746
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-05-31 18:46:58 -0400

    Add enforcer.sh to the toad.tgz archive

--- ansible/files/make_toad.sh
+++ ansible/files/make_toad.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar czvf toad.tgz /etc/rc.d/gate /etc/rc.d/*web /etc/rc.d/re3 /etc/pf.conf /etc/ssh/sshd_config /srv /opt/local /opt/kiagateway /opt/*bluejay/ /usr/local/bin/*bluejay /root/*wrap /root/certbot* make_toad.sh
+tar czvf toad.tgz /etc/rc.d/gate /etc/rc.d/*web /etc/rc.d/re3 /etc/pf.conf /etc/ssh/sshd_config /srv /opt/local /opt/kiagateway /opt/*bluejay/ /usr/local/bin/*bluejay /root/*wrap /root/certbot* /root/enforcer.sh /root/make_toad.sh
