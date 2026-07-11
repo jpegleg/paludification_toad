fa8f563ecf15eee22825b99b1059e79733374c86
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-07-11 00:04:50 -0400

    update terraform for openbsd 7.9

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ resource "vultr_ssh_key" "enre" {
   ssh_key = "YOUR PUBLIC KEY GOES HERE"
 }
 
-resource "vultr_instance" "toad" {
-    hostname = "bog1"
+resource "vultr_instance" "gtd1" {
+    hostname = "gtd1"
     plan = "vc2-1c-1gb"
     region = "atl"
-    os_id = 2679
+    os_id = 2772
     ssh_key_ids = ["${vultr_ssh_key.enre.id}"]
-    label = "toad"
+    label = "gtd1"
 }
